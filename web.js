a518ae4df78fd40c0cf9ef09f5feec9bd1c82b81
Chris Bisignani <chris.bisignani@addepar.com>
2013-07-09 11:56:55 -0700

    WIP

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
 app.get('/thing', function (req, res) {
-  response.send('You are thing!');
+  res.send('You are thing!');
 });
 
 var port = process.env.PORT || 5000;
